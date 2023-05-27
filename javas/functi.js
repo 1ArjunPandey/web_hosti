92a84cc1fd6962a2e25798188c3d3ce511c416b3
1ArjunPandey <my.arjunpandey@gmail.com>
2023-05-27 18:32:15 +0530

    Website host karta

---
+++ javas/functi.js
@@ -0,0 +1,18 @@
+
+
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+nav_list = document.querySelector('.nav-listt');
+rightNav = document.querySelector('.rightNav');
+
+console.log(nav_list);
+
+
+
+burger.addEventListener('click', ()=>{
+
+    rightNav.classList.toggle('hid-kar');
+    nav_list.classList.toggle('hid-kar');
+    navbar.classList.toggle('h-nav-resp');
+})
+
